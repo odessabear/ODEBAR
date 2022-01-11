392c09750567e4be7ca8b03af322d47df5adacf7
odessabear <bearodessa@gmail.com>
2022-01-11 02:07:04 +0200

    added pow function

--- src/com/odebar/git/Launcher.java
+++ src/com/odebar/git/Launcher.java
@@ -6,6 +6,8 @@ public class Launcher {
         int b = 3;
         System.out.println(add(a, b));
         System.out.println(sub(a, b));
+        System.out.println(multiply(a, b));
+        System.out.println(pow(a, b));
     }
 
     private static int add(int a, int b) {
@@ -14,5 +16,13 @@ public class Launcher {
 
     private static int sub(int a, int b) {
         return a - b;
+    }
+
+    private static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    private static int pow(int a, int b) {
+        return (int) Math.pow(a, b);
     }
 }
