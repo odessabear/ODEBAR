29f3e084bc586b2d282f402a858face8f7e73c46
odessabear <bearodessa@gmail.com>
2022-01-11 02:13:32 +0200

    added min function

--- src/com/odebar/git/Launcher.java
+++ src/com/odebar/git/Launcher.java
@@ -8,6 +8,7 @@ public class Launcher {
         System.out.println(sub(a, b));
         System.out.println(multiply(a, b));
         System.out.println(pow(a, b));
+        System.out.println("minimal value from " + a + " and " + b + " is " + min(a, b));
     }
 
     private static int add(int a, int b) {
@@ -24,5 +25,9 @@ public class Launcher {
 
     private static int pow(int a, int b) {
         return (int) Math.pow(a, b);
+    }
+
+    private static int min(int a, int b) {
+        return Math.min(a, b);
     }
 }
