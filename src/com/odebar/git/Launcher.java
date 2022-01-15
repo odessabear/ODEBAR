158936d7a804569c70cd738fe1651c1673a0a895
odessabear <bearodessa@gmail.com>
2022-01-15 02:37:58 +0200

    commit

--- src/com/odebar/git/Launcher.java
+++ src/com/odebar/git/Launcher.java
@@ -9,6 +9,7 @@ public class Launcher {
         System.out.println(multiply(a, b));
         System.out.println(pow(a, b));
         System.out.println("minimal value from " + a + " and " + b + " is " + min(a, b));
+
     }
 
     private static int add(int a, int b) {
